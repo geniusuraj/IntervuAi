4b636274ddd83353e012828beb90ae522603d505
Rutwik Kumbhar <kumbharrutwik@gmail.com>
2023-09-01 18:39:48 +0530

    java controller layer done

---
+++ Backend/IntervuAi/src/main/java/com/IntervuAi/DTO/Answer.java
@@ -0,0 +1,14 @@
+package com.IntervuAi.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Answer {
+	
+	private String question;
+	private String answer;
+}
---
+++ Backend/IntervuAi/src/main/java/com/IntervuAi/DTO/FeedBack.java
@@ -0,0 +1,15 @@
+package com.IntervuAi.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedBack {
+	private Integer  subjectExperties;
+	private Integer communicationSkill;
+	private Integer problemSolving;
+
+}
