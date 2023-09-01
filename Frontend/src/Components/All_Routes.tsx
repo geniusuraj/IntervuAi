966145dec8fe2c16fed5a0fb36e3d9e35d182c1c
MouS0926 <moupriya2609@gmail.com>
2023-09-01 23:54:07 +0530

    speech recognizer

--- Frontend/src/Components/All_Routes.tsx
+++ Frontend/src/Components/All_Routes.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Category from "../Pages/Category";
+import Speech from "./Speech";
 const All_Routes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Category/>} />
+        <Route path="/node" element={<Speech/>} />
       </Routes>
     </div>
   );
