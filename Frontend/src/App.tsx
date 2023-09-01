932c536bfd83f020bebaca55084a14be45f18f64
Mohnish Vishwakarma <mohnish201@gmail.com>
2023-09-01 17:57:40 +0530

    Router created

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import All_Routes from './Components/All_Routes';
+import Category from './Pages/Category';
 
 function App() {
   return (
     <div className="App">
-           <All_Routes/>
+           {/* <All_Routes/> */}
+           <Category/>
     </div>
   );
 }
